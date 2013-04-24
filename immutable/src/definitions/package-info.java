df2cd707ae08e8f85cf23cdcd4e12458e7552e54
duga9cu <asinochilegge87@yahoo.it>
2013-04-24 12:50:58 +0200

    javadoc in the repo

--- immutable/src/definitions/package-info.java
+++ immutable/src/definitions/package-info.java
@@ -16,7 +16,7 @@
  * 
  *  			FUNCTIONING OF THE SOFTWARE:
  * 		- the client instantiate a class which implements the interface Immutable -
- * 		- the implementation of the function validateMyself must return a List<Immutable> object containing two copies of the object itself -
+ * 		- the implementation of the function validateMyself() must return the object itself -
  * 		- the client then sends the object to validate to the server -
  * 		- the server then accept the client's object and pass it to a thread - 
  * 		- the serverThread checks using reflection and processors whether the object is really immutable - 
